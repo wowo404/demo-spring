038c51ed16be928dc49d7129c19d376c5735f96f
LiuZhangsheng <1879036165@qq.com>
2022-04-13 23:04:35 +0800

    demo

---
+++ demo-springboot-shiro/src/test/java/org/liu/demo/DemoSpringbootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
