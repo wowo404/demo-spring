31d8b8222ac3a7269b5b7cc3a8b131f1bbccc512
刘章盛 <hello-ketty-honey@163.com>
2022-04-22 17:27:49 +0800

    finish

---
+++ demo-spring-springmvc-mybatis/src/main/java/org/liu/ssm/filters/MyFilter.java
@@ -0,0 +1,35 @@
+package org.liu.ssm.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * @Author lzs
+ * @Date 2022/4/22 10:42
+ **/
+@Component
+@Slf4j
+public class MyFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        FilterConfig filterConfig = getFilterConfig();
+        //filterConfig是Nullable的，这里是idea误报了
+        if (null != filterConfig) {
+            String params = String.join(",", Collections.list(filterConfig.getInitParameterNames()));
+            log.info("当前过滤器名字:{}, 参数:{}", filterConfig.getFilterName(), params);
+        }
+        log.info("before execute next filter");
+        chain.doFilter(request, response);
+        log.info("after execute next filter");
+    }
+
+    @Override
+    protected void initFilterBean() throws ServletException {
+        super.initFilterBean();
+    }
+}
