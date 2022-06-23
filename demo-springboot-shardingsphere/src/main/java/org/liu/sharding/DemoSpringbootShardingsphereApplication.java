343dc528633c05c0926219027f3a8c68222ee43f
刘章盛 <hello-ketty-honey@163.com>
2022-06-23 16:40:17 +0800

    finish

---
+++ demo-springboot-shardingsphere/src/main/java/org/liu/sharding/DemoSpringbootShardingsphereApplication.java
@@ -0,0 +1,13 @@
+package org.liu.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootShardingsphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootShardingsphereApplication.class, args);
+    }
+
+}
