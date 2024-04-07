04c491b654e76b1233859e3543a621d8bb46a8ab
刘章盛 <hello-ketty-honey@163.com>
2024-04-07 17:41:04 +0800

    finish

---
+++ demo-springboot-mongodb/src/main/java/org/liu/demo/mongodb/pojo/InventorySize.java
@@ -0,0 +1,14 @@
+package org.liu.demo.mongodb.pojo;
+
+import lombok.Data;
+
+/**
+ * @author lzs
+ * @Date 2024/4/7 17:13
+ **/
+@Data
+public class InventorySize {
+    private Double h;
+    private Double w;
+    private String uom;
+}
